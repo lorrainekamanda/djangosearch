a4c772b2489ada0cd83cd131628cc01771c9661f
lorrainekamanda <lorrainekamanda@gmail.com>
2020-04-26 02:03:25 +0300

    adding the source test

--- Desktop/news/app/models/news_test.py
+++ Desktop/news/app/models/news_test.py
@@ -1,6 +1,6 @@
 import unittest
 from news import News
-
+from news import Source
 
 class NewsTest(unittest.TestCase):
 
@@ -12,11 +12,11 @@ class NewsTest(unittest.TestCase):
       "https://www.coindesk.com/wp-content/uploads/2020/04/Coldie-Julian-Assange-Decentral-Eyes-Gold-Edition-1_1-1200x628.jpg",
       "2020-04-22T18:49:06Z",'With the lights turned off')
 
-   
+      self.new_source = Source(1,'Coindesk.com')
 
   def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
-       
+        self.assertTrue(isinstance(self.new_source,Source))
     
 
 
