a7c6b4e88cf143362dd6a6554db67db6eac1d6f4
lorrainekamanda <lorrainekamanda@gmail.com>
2020-04-26 01:54:29 +0300

    adding News Model

---
+++ Desktop/news/app/models/news.py
@@ -0,0 +1,21 @@
+class News:
+   def __init__(self,author,source,title,description,url,urlToImage,publishedAt,content):
+
+      
+       self.author = author
+       self.source = source
+       self.title = title
+       self.description = description
+       self.url = url
+       self.description = description
+       self.urlToImage = urlToImage
+       self.publishedAt = publishedAt
+       self.content = content
+
+
+
+
+    
+
+
+
